f889632bc01881c3434536adb2eb84299778768c
shocodeart <shocodeart@gmail.com>
2022-10-29 22:21:52 +0500

    extend admin

--- posts/admin.py
+++ posts/admin.py
@@ -5,4 +5,17 @@ from posts.models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "get_text", "is_draft")
     readonly_fields = ("created_at", "updated_at")
+    list_editable = ("is_draft",)
+    search_fields = (
+        "title",
+        "text",
+        "author__username",
+        "author__first_name",
+        "author__last_name",
+    )
+    list_filter = ("is_draft",)
+
+    def get_text(self, instance):
+        return f"{instance.text:.300}"
