50f9b0312c3d8ef2d21b2ec11b08bb6dc2b4e702
shocodeart <shocodeart@gmail.com>
2022-11-01 18:38:10 +0500

    add tags

--- posts/admin.py
+++ posts/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
-from posts.models import Post
+from posts.models import Post, Tag
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    filter_horizontal = ("tags",)
     list_display = ("__str__", "get_text", "is_draft")
     readonly_fields = ("created_at", "updated_at")
     list_editable = ("is_draft",)
--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.utils import timezone
 
 
+class Tag(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return f"{self.title:.30}"
+
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
@@ -10,6 +17,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     published_at = models.DateTimeField(null=True, blank=True)
+    tags = models.ManyToManyField("posts.Tag")
 
     def __str__(self):
         return f"{self.title:.30}"
